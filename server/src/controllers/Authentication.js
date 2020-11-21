5cabc96592ec1b7a2370440f963be41280beed2e
Rachel Lewis <60766726+RLewis627@users.noreply.github.com>
2020-11-20 21:10:30 -0800

    Add files via upload

--- server/src/controllers/Authentication.js
+++ server/src/controllers/Authentication.js
@@ -6,9 +6,7 @@ module.exports = {
       const user = await users.create(req.body)
       res.send(user.toJSON())
     } catch (err) {
-      res.status(400).send({
-        error: 'Something bad happened.'
-      })
+      res.status(400).send(err)
     }
   }
 }
